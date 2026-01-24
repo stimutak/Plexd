6957e5ce30cc1cdcc8a51d7d4a3dc938aa7ea149
Oliver Delano <oed@mac.com>
2026-01-24 01:25:37 -0500

    Bump service worker cache version

--- web/sw.js
+++ web/sw.js
@@ -1,5 +1,5 @@
 // Plexd Service Worker - Caches UI for offline use
-const CACHE_NAME = 'plexd-remote-v3';
+const CACHE_NAME = 'plexd-remote-v4';
 const ASSETS = [
     '/remote.html',
     '/css/remote.css',
